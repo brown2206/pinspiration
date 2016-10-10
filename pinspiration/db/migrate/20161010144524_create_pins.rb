049109c249ca7be6fa14155f3db2353f7c573d5d
brown2206 <brown.angelo.jr@gmail.com>
2016-10-10 11:37:04 -0400

    database created

---
+++ pinspiration/db/migrate/20161010144524_create_pins.rb
@@ -0,0 +1,8 @@
+class CreatePins < ActiveRecord::Migration[5.0]
+  def change
+    create_table :pins do |t|
+      t.string :title
+      t.string :img_url
+    end
+  end
+end
--- pinspiration/db/seeds.rb
+++ pinspiration/db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Pin.destroy_all
+# Comment.destroy_all
+
+Pin.create([
+  {title: "Batman",img_url: "http://a1.mzstatic.com/us/r1000/003/Music/db/24/b1/mzi.wevryeas.200x200-75.jpg" }
+])
+
+# new comment
+batman = Pin.first
+batman.comments.create(body: "Hi", created_at: "Today")
